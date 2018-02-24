c79b0b2cd0d1fc80c2507d7fb5f7b6cc3683ba5a
Wingasha <wingasha@gmail.com>
2018-02-24 19:26:47 +0200

    Setting up the frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'products',
 ]
 
@@ -50,9 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
@@ -121,7 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "products/static"),
+)
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,6 +2,10 @@ from rest_framework import routers
 
 from .views import ProductViewSet
 
+from django.conf.urls import url
+
+app_name = 'products'
+
 # Создаем router и регистрируем наш ViewSet
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
